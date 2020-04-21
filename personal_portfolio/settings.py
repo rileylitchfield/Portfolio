608850bce45d391b311f9e96ac7fe5e4aced1101
rileylitchfield <rileylitchfield95@gmail.com>
2020-04-20 20:49:19 -0500

    updated aws

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -31,15 +31,7 @@ DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 ALLOWED_HOSTS = ['https://rileyportfolio.herokuapp.com/']
 
 # amazon s3
-AWS_S3_REGION_NAME = 'us-east-2'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Application definition
 
